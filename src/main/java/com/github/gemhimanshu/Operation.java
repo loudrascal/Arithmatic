9e89ba6cb115fad5f45d3321f2c6a8942efcfb3b
himanshu <himanshu142739@gmail.com>
2021-05-12 02:18:10 +0530

    First Commit

---
+++ src/main/java/com/github/gemhimanshu/Operation.java
@@ -0,0 +1,11 @@
+package com.github.gemhimanshu;
+
+public class Operation {
+    public static int add(int a, int b){return a+b;}
+
+    public static int subtract(int a, int b){return a-b;}
+
+    public static int multiply(int a, int b){return a*b;}
+
+    public static int divide(int a, int b){return a/b;}
+}
